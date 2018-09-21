ddd1091b50558bca42d646f8082f36d52bc19645
ZhaoJiafeng <774357165@qq.com>
2018-09-21 13:42:04 +0800

    task2

--- task2/script/script.js
+++ task2/script/script.js
@@ -6,9 +6,38 @@ function back(){
 }
 function get_num(){
     var n = document.getElementById("num").value;
-    if(n<=0){
+    if(n<5){
         alert("请输入有效人数");
+        return 0;
     }else{
-        alert(n);
+        return n;
     }
-} 
+}
+function march(){
+    var n = get_num();
+    var killer;
+    var police;
+    var shooter = 1;
+    var doctor = 1;
+    var man;
+    if(n<5){
+        return 0;
+    }else if(n<9){
+        killer = 1;
+    }else if(n<12){
+        killer = Math.ceil(n/6);
+    }else if(n<15){
+        killer = Math.ceil(n/5);
+    }else{
+        killer = Math.ceil(n/4);
+    }
+    alert(killer);
+    if(n<9){
+        police = 1;
+    }else{
+        police = killer - 1;
+    }
+    alert(police);
+    man = n - killer - police -shooter - doctor;
+    alert(man);
+}
